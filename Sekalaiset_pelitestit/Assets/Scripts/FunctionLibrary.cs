39a628e9fae15cbfc3f978d34284e7cc7a684464
DESKTOP-B67THC4\sinik <sini.karhunen@gmail.com>
2020-12-20 22:25:39 +0200

    Start

---
+++ Sekalaiset_pelitestit/Assets/Scripts/FunctionLibrary.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Mathf;
+
+public static class FunctionLibrary {
+
+    public delegate float Function(float x, float t);
+
+    public static Function GetFunction(int index) {
+        if (index == 0) {
+            return Wave;
+        } else if (index == 1) {
+            return MultiWave;
+        } else {
+            return Ripple;
+        }
+    }
+
+    //to make them work directly at the class level we have to mark it as static
+    public static float Wave(float x, float t) {
+        return Sin(PI * (x + t)); 
+    }
+
+    public static float MultiWave ( float x, float t) {
+        float y = Sin(PI * (x + 2.5f * t));
+        y += Sin(2f * PI * (x + t)) * 0.5f;
+        return y * (2f / 3f );
+    }
+
+    public static float Ripple ( float x, float t) {
+        float d = Abs(x);
+        float y = Sin(PI * (4f * d - t));
+        return y / (1f + 10f * d);
+    }
+
+}
+
+
+
---
+++ Sekalaiset_pelitestit/Assets/Scripts/Graph.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform pointPrefab = default;
+
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+
+    [SerializeField, Range(0, 1)]
+    int function = 0;
+
+
+    Transform[] points; 
+
+    private void Awake() {
+        float step = 2f / resolution;
+        var position = Vector3.zero; 
+        var scale = Vector3.one * step ;
+        points = new Transform[resolution];
+        for( int i = 0; i < points.Length; i++) {
+
+            Transform point = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
+            //position.y = position.x * position.x * position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+
+            points[i] = point; 
+            point.SetParent(transform, false);
+        }
+ 
+    }
+
+
+
+    void Update()
+    {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function); 
+        float time = Time.time * 0.5f;
+        for (int i = 0; i < points.Length; i++) {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            if (function == 0) {
+                position.y = FunctionLibrary.Wave(position.x, time);
+                //position.y = FunctionLibrary.Wave(position.x, time);
+                //position.y = Mathf.Cos(Mathf.PI * (position.x + time));
+                //position.y = (position.x * position.x * position.x) ;
+            } else {
+                position.y = FunctionLibrary.Ripple(position.x, time);
+            }
+            point.localPosition = position;
+        }
+    }
+}
